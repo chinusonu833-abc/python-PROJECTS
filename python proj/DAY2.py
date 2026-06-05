fc2e534f08aa0f6ad48c2f972f951803f467c591
Chinmai SV <chinusonu833@gmail.com>
2026-06-05 23:07:57 +0530

    Add files via upload

---
+++ python proj/DAY2.py
@@ -0,0 +1,15 @@
+name = input ("Enter your Name:");
+age = input("Enter your age:")
+
+print ("HEEELO!",name)
+print("you are",age )
+
+num1 = int (input ("Enter a 1st number:"))
+num2 = int (input ("Enter the 2nd number:"))
+
+sub = num1 - num2
+print("THE ANSWER IS:", sub)
+
+
+sum = num1 +num2
+print("THE ANSWER IS :" ,sum)
